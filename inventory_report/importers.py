d41bda24b6286fd2464ad7b0f1e22b81a3d9a2e7
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-23 23:30:31 -0400

    Feat: Requisito 03

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,16 +1,63 @@
+from abc import ABC, abstractmethod
+import csv
+import json
 from typing import Dict, Type
 
-
-class Importer:
-    pass
+from inventory_report.product import Product
 
 
-class JsonImporter:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> list[Product]:
+        ...
 
 
-class CsvImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        data_import = list()
+
+        with open(self.path) as file:
+            products_path = json.load(file)
+
+        for i in products_path:
+            data_import.append(
+                Product(
+                    i["id"],
+                    i["product_name"],
+                    i["company_name"],
+                    i["manufacturing_date"],
+                    i["expiration_date"],
+                    i["serial_number"],
+                    i["storage_instructions"],
+                )
+            )
+        return data_import
+
+
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        data_import = list()
+
+        with open(self.path, encoding="utf-8") as file:
+            information = csv.reader(file, delimiter=",", quotechar='"')
+            header, *products_path = information
+
+        for i in products_path:
+            data_import.append(
+                Product(
+                    i[0],
+                    i[1],
+                    i[2],
+                    i[3],
+                    i[4],
+                    i[5],
+                    i[6],
+                )
+            )
+        return data_import
 
 
 # Não altere a variável abaixo
