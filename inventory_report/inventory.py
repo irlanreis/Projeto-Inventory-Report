3ad470db9cbb50bab28b466f67dbaa270ea1f874
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-24 09:55:44 -0400

    Feat: Requisito 05

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        self._data = data if data else []
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data.copy()
+
+    def add_data(self, data: list[Product]) -> None:
+        self._data.extend(data)
