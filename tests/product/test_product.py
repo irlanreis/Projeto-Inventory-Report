d6178a7ae21250ccd252db6b3b6940d6009de115
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-23 22:41:49 -0400

    Inicia Projeto :rocket: Feat: Requisito 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,29 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    id = "2"
+    product = "Popper-ram"
+    company = "Company_example"
+    manufacturing = "2023-10-23"
+    expiration = "2023-10-24"
+    serial = "789"
+    storage = "Pesca_Esportiva"
+
+    product_ex = Product(
+        id,
+        product,
+        company,
+        manufacturing,
+        expiration,
+        serial,
+        storage,
+    )
+
+    assert product_ex.id == id
+    assert product_ex.product_name == product
+    assert product_ex.company_name == company
+    assert product_ex.manufacturing_date == manufacturing
+    assert product_ex.expiration_date == expiration
+    assert product_ex.serial_number == serial
+    assert product_ex.storage_instructions == storage
